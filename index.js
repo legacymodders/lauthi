4088f6c4dc21a5c18bfbcc73fd25ebc7cb92adb6
legacymodders <129878298+legacymodders@users.noreply.github.com>
2023-04-04 17:03:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 80
+
+const showHWID = true;
+let whitelist;
+
+const fs = require("fs");
+try {
+    let jsonString = fs.readFileSync("./whitelist.json");
+    whitelist = JSON.parse(jsonString)[0];
+} catch(err) {
+    console.log("ERROR: no whitelist file found! shutting lauthi down.")
+    process.exit(1);
+}
+
+let users = Object.keys(whitelist);
+let HWIDs = Object.values(whitelist);
+
+
+
+app.get("/", (req, res) => {
+    if (showHWID) {
+        console.log(req.headers["sw-fingerprint"])
+    }
+    
+    let HWID = req.headers["sw-fingerprint"];
+
+    for (let i = 0; i < users.length; i++) {
+        if (HWID == HWIDs[i]) {
+            res.send([true, users[i]]);
+            return;
+        }
+    }
+
+    res.send([false, "whitelist failed."])
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
