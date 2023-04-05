8872d2395a00ccb77c4b23840b624216feca979c
legacymodders <129878298+legacymodders@users.noreply.github.com>
2023-04-05 14:44:29 +0100

    added synapse support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 80
 
 const showHWID = true;
 let whitelist;
@@ -16,18 +16,51 @@ try {
 
 let users = Object.keys(whitelist);
 let HWIDs = Object.values(whitelist);
+let HWID;
+let syn;
+let sw;
 
 app.get("/", (req, res) => {
-    if (showHWID) {
-        console.log(req.headers["sw-fingerprint"])
+    if (!(req.headers["sw-fingerprint"] == undefined)) {
+        HWID = req.headers["sw-fingerprint"];
+        sw = true;
+        syn = false;
+    } else if (!(req.headers["syn-fingerprint"] == undefined)) {
+        HWID = req.headers["syn-fingerprint"];
+        syn = true;
+        sw = false;
+    } else {
+        HWID = undefined;
     }
-    
-    let HWID = req.headers["sw-fingerprint"];
+
+    if (showHWID) {
+        console.log(HWID);
+    }
 
     for (let i = 0; i < users.length; i++) {
         if (HWID == HWIDs[i]) {
             res.send([true, users[i]]);
             return;
+        }
+
+        if (sw) {
+            if (!(HWIDs[i][0] == "")) {
+                console.log("SCRIPTWARE");
+                if (HWID == HWIDs[i][0]) {
+                    res.send([true, users[i]]);
+                    return;
+                }
+            }
+        }
+    
+        if (syn) {
+            if (!(HWIDs[i][1] == "")) {
+                console.log("SYNAPSE");
+                if (HWID == HWIDs[i][1]) {
+                    res.send([true, users[i]]);
+                    return;
+                }
+            }
         }
     }
 
