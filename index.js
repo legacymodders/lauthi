42e310498409cf23af5b00736d25b1381e4455b0
legacymodders <129878298+legacymodders@users.noreply.github.com>
2023-04-05 15:36:14 +0100

    added KRNL support

--- index.js
+++ index.js
@@ -16,19 +16,30 @@ try {
 
 let users = Object.keys(whitelist);
 let HWIDs = Object.values(whitelist);
-let HWID;
-let syn;
-let sw;
+let HWID = "";
+let syn = false;
+let sw = false;
+let krnl = false;
 
 app.get("/", (req, res) => {
     if (!(req.headers["sw-fingerprint"] == undefined)) {
         HWID = req.headers["sw-fingerprint"];
         sw = true;
         syn = false;
+        krnl = false;
+
     } else if (!(req.headers["syn-fingerprint"] == undefined)) {
         HWID = req.headers["syn-fingerprint"];
         syn = true;
         sw = false;
+        krnl = false;
+
+    } else if (!(req.headers["krnl-hwid"] == undefined)) {
+        HWID = req.headers["krnl-hwid"];
+        krnl = true;
+        syn = false;
+        sw = false;
+
     } else {
         HWID = undefined;
     }
@@ -57,6 +68,16 @@ app.get("/", (req, res) => {
             if (!(HWIDs[i][1] == "")) {
                 console.log("SYNAPSE");
                 if (HWID == HWIDs[i][1]) {
+                    res.send([true, users[i]]);
+                    return;
+                }
+            }
+        }
+
+        if (krnl) {
+            if (!(HWIDs[i][2] == "")) {
+                console.log("KRNL");
+                if (HWID == HWIDs[i][2]) {
                     res.send([true, users[i]]);
                     return;
                 }
